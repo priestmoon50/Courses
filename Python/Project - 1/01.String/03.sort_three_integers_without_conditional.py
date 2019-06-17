48221e7188d5250ef79a74b57b9949df2f59c6fd
Smoke <33788011+priestmoon50@users.noreply.github.com>
2019-06-17 02:02:47 -0700

    change idea

--- Python/Project - 1/01.String/03.sort_three_integers_without_conditional.py
+++ Python/Project - 1/01.String/03.sort_three_integers_without_conditional.py
@@ -2,9 +2,7 @@
 x = int(input("Input first number : "))
 y = int(input("Input second number : "))
 z = int(input("Input third number : "))
+d = (x,y,z)
+d.sort()
 
-
-a1 = min(x, y, z)
-a3 = max(x, y, z)
-a2 = (x + y + z) - a1 - a3
-print("Numbers in sorted order: ", a1, a2, a3)
+print(d)
